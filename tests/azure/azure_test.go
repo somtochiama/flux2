2287a37546139941e65bfaf154811302b139b10f
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2022-11-22 18:25:05 +0100

    Use go-git instead
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- tests/azure/azure_test.go
+++ tests/azure/azure_test.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2021 The Flux authors
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -905,7 +902,6 @@ func TestEventHubNotification(t *testing.T) {
 // TODO: Enable when source-controller supports Helm charts from OCI sources.
 /*func TestACRHelmRelease(t *testing.T) {
 	ctx := context.TODO()
-
 	// Create namespace for test
 	namespace := corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
@@ -918,7 +914,6 @@ func TestEventHubNotification(t *testing.T) {
 		kubeClient.Delete(ctx, &namespace)
 		require.NoError(t, err)
 	}()
-
 	// Copy ACR credentials to new namespace
 	acrNn := types.NamespacedName{
 		Name:      "acr-helm",
@@ -933,7 +928,6 @@ func TestEventHubNotification(t *testing.T) {
 	}
 	err = kubeClient.Create(ctx, &acrSecret)
 	require.NoError(t, err)
-
 	// Create HelmRepository and wait for it to sync
 	helmRepository := sourcev1.HelmRepository{
 		ObjectMeta: metav1.ObjectMeta{
