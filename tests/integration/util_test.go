5651e2ec519a2762969f0a18cc7a3c5e356c5694
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2023-03-22 23:10:42 +0100

    print source condition
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- tests/integration/util_test.go
+++ tests/integration/util_test.go
@@ -163,7 +163,8 @@ func verifyGitAndKustomization(ctx context.Context, kubeClient client.Client, na
 	}
 
 	if apimeta.IsStatusConditionPresentAndEqual(source.Status.Conditions, meta.ReadyCondition, metav1.ConditionTrue) == false {
-		return fmt.Errorf("source condition not ready, %v", source.Status.Conditions)
+		fmt.Println(source.Status)
+		return fmt.Errorf("source condition not ready")
 	}
 	kustomization := &kustomizev1.Kustomization{}
 	err = kubeClient.Get(ctx, nn, kustomization)
